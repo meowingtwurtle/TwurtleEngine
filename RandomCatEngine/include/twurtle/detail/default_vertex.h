953ca6d8484896250886b6bbf5e66d3d365aef48
Jason Cobb <jason.e.cobb@gmail.com>
2018-09-03 19:12:01 -0400

    Add vertex struct

---
+++ RandomCatEngine/include/twurtle/detail/default_vertex.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/glm.hpp>
+
+namespace randomcat::graphics {
+    struct default_vertex {
+        glm::vec3 location;
+    };
+}    // namespace randomcat::graphics
