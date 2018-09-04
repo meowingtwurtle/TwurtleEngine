59b26ff7024c68812235435496d841276a192519
Jason Cobb <jason.e.cobb@gmail.com>
2018-09-04 16:57:10 -0400

    Update renderer spec with macro

--- RandomCatEngine/include/randomcat/engine/graphics/renderer.h
+++ RandomCatEngine/include/randomcat/engine/graphics/renderer.h
@@ -10,10 +10,18 @@ namespace randomcat::engine::graphics {
         virtual void makeActive() const = 0;
     };
 
-    template<typename _vertex_t>
-    class vertex_renderer : public renderer {
-    public:
-        virtual std::vector<_vertex_t>& vertices() = 0;
-        virtual std::vector<_vertex_t> const& vertices() const = 0;
+    // Using macro to highlight that replacements are the same thing, just with
+    // different words
+
+#define RENDERER_SPEC(singular, plural)                                                                                                    \
+    template<typename _##singular##_t>                                                                                                     \
+    class singular##_renderer : public renderer {                                                                                          \
+    public:                                                                                                                                \
+        virtual std::vector<_##singular##_t>& plural() = 0;                                                                                \
+        virtual std::vector<_##singular##_t> const& plural() const = 0;                                                                    \
     };
+
+    RENDERER_SPEC(vertex, vertices)
+
+#undef RENDERER_SPEC
 }    // namespace randomcat::engine::graphics
