507f8341811c50520eaf4c3dd5ffdf729001fa52
Jason Cobb <jason.e.cobb@gmail.com>
2018-10-05 16:40:25 -0400

    Use value semantics instead of shared_ptrs in default_object_renderer

--- RandomCatEngine/include/randomcat/engine/graphics/object_renderer.h
+++ RandomCatEngine/include/randomcat/engine/graphics/object_renderer.h
@@ -9,18 +9,16 @@
 
 namespace randomcat::engine::graphics {
     template<typename _object_t, typename _renderer_t = detail::default_renderer<typename _object_t::component::vertex>>
-    class default_object_renderer : public object_renderer<std::shared_ptr<_object_t>> {
+    class default_object_renderer : public object_renderer<_object_t> {
     public:
         using object = _object_t;
         using component = typename object::component;
         using vertex = typename component::vertex;
         using renderer = _renderer_t;
 
-        using object_ptr = std::shared_ptr<_object_t>;
-
         virtual void render() const override {
             for (auto const& object : objects()) {
-                auto const components = object->components();
+                auto const components = object.components();
 
                 std::vector<vertex> vertices;
 
@@ -29,16 +27,16 @@ namespace randomcat::engine::graphics {
                     vertices.insert(std::end(vertices), std::begin(subVertices), std::end(subVertices));
                 }
 
-                auto vertexRenderer = renderer{object->shader()};
+                auto vertexRenderer = renderer{object.shader()};
                 vertexRenderer.vertices() = std::move(vertices);
                 vertexRenderer.render();
             }
         }
 
-        virtual std::vector<object_ptr>& objects() override { return m_objects; }
-        virtual std::vector<object_ptr> const& objects() const override { return m_objects; }
+        virtual std::vector<object>& objects() override { return m_objects; }
+        virtual std::vector<object> const& objects() const override { return m_objects; }
 
     private:
-        std::vector<object_ptr> m_objects;
+        std::vector<object> m_objects;
     };
 }    // namespace randomcat::engine::graphics
