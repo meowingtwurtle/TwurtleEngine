7b3e5012ef3115d7f656d5d5dea97f021bb94043
Jason Cobb <jason.e.cobb@gmail.com>
2018-12-15 23:33:18 -0500

    Add warning for double initialization of graphics

--- RandomCatEngine/src/graphics/init.cpp
+++ RandomCatEngine/src/graphics/init.cpp
@@ -4,7 +4,15 @@
 #include <randomcat/engine/graphics/init.hpp>
 
 namespace randomcat::engine::graphics {
+    namespace {
+        bool g_initDone = false;
+    }
+
     void init() {
+        if (g_initDone) { log::warn("Graphics init called when already initialized!"); }
+
+        g_initDone = true;
+
         log::info("Beginning graphics initialization...");
 
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
