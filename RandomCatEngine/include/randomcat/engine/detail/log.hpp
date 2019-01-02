28106983f5068ad6cb934435c170f7cd041af7f0
Jason Cobb <jason.e.cobb@gmail.com>
2019-01-02 15:05:50 -0500

    More logging changes

--- RandomCatEngine/include/randomcat/engine/detail/log.hpp
+++ RandomCatEngine/include/randomcat/engine/detail/log.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <chrono>
 #include <functional>
 #include <iomanip>
@@ -9,24 +10,28 @@
 namespace randomcat::engine::log {
     enum class log_type : uint8_t { INFO, WARN, ERROR };
 
-    inline std::string log_type_name(log_type _logType) noexcept {
-        switch (_logType) {
-            case log_type::INFO: return "INFO";
-            case log_type::WARN: return "WARN";
-            case log_type::ERROR: return "ERROR";
+    namespace detail {
+        inline std::atomic<std::reference_wrapper<std::ostream>> g_logStream = std::atomic<std::reference_wrapper<std::ostream>>(std::clog);
+        inline std::atomic_flag g_isNotFirstLog = ATOMIC_FLAG_INIT;
+
+        inline std::string log_type_header(log_type _logType) noexcept {
+            switch (_logType) {
+                case log_type::INFO: return "[INFO]";
+                case log_type::WARN: return "[WARN]";
+                case log_type::ERROR: return "[ERROR]";
+            }
+
+            return "[INVALID]";
         }
 
-        return std::string{"Invalid log type: "} + std::to_string(static_cast<std::underlying_type_t<decltype(_logType)>>(_logType));
-    }
-
-    namespace detail {
-        inline std::reference_wrapper<std::ostream> g_logStream = std::clog;
-
+        // Client code shall not use this type except as the result of library-provided
+        // functions, even if it is the result of a decltype or auto variable. Client
+        // code shall not store any instance of this type.
         class log_impl {
         public:
             enum class cont_type { first, continued };
 
-            explicit log_impl(log_type _logType, cont_type _contType = cont_type::first)
+            explicit log_impl(log_type _logType, cont_type _contType = cont_type::first) noexcept
             : m_logType(std::move(_logType)), m_contType(std::move(_contType)) {}
 
             log_impl(log_impl const&) = delete;
@@ -35,28 +40,35 @@ namespace randomcat::engine::log {
             log_impl operator=(log_impl&&) = delete;
 
             template<typename T>
-            log_impl operator<<(T const& _val) noexcept(false) {
-                std::ostringstream tempOut;
+            log_impl operator<<(T const& _val) noexcept {
+                auto& outStream = g_logStream.load(std::memory_order_acquire).get();
 
-                bool const isFirst = (m_contType == cont_type::first);
+                try {
+                    std::ostringstream tempOut;
 
-                if (isFirst)
-                    tempOut << log_header();
-                else {
-                }; /* continued, don't output header */
+                    bool const isFirst = (m_contType == cont_type::first);
 
-                tempOut << _val;
+                    if (isFirst)
+                        tempOut << log_header();
+                    else {
+                    }; /* continued, don't output header */
 
-                std::string outString = std::move(tempOut).str();
+                    tempOut << _val;
 
-                replace_newlines(outString, log_header_cont());
+                    std::string outString = std::move(tempOut).str();
 
-                (g_logStream.get()) << (isFirst ? "\n" : "") << outString;
+                    replace_newlines(outString, log_header_cont());
+
+                    outStream << ((isFirst && g_isNotFirstLog.test_and_set()) ? "\n" : "") << outString;
+                } catch (...) {
+                    // Drop error, logging is not vital
+                    outStream << "<LOG ERROR>";
+                }
 
                 return log_impl(m_logType, cont_type::continued);
             }
 
-            void replace_newlines(std::string& _str, std::string const& _replacement) const {
+            void replace_newlines(std::string& _str, std::string const& _replacement) const noexcept(false) {
                 auto newLinePos = _str.find('\n');
 
                 while (newLinePos != std::string::npos) {
@@ -66,7 +78,7 @@ namespace randomcat::engine::log {
             }
 
             template<typename T>
-            void operator()(T const& _val) noexcept(false) {
+            void operator()(T const& _val) noexcept {
                 (*this) << _val;
             }
 
@@ -75,7 +87,8 @@ namespace randomcat::engine::log {
                 std::ostringstream fullMessage{};
                 time_t systemTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
                 auto formatTime = *std::localtime(&systemTime);
-                fullMessage << "[" << std::put_time(&formatTime, "%Y-%m-%d %H:%M:%S") << "] [" << log_type_name(m_logType) << "] ";
+
+                fullMessage << "[" << std::put_time(&formatTime, "%Y-%m-%d %H:%M:%S") << "] " << log_type_header(m_logType) << " ";
 
                 return fullMessage.str();
             }
@@ -87,7 +100,15 @@ namespace randomcat::engine::log {
         };
     }    // namespace detail
 
-    inline detail::log_impl log(log_type _type) noexcept { return detail::log_impl(std::move(_type)); }
+    // Client code shall not store any result of this function
+    // Guaranteed to be of a type such that:
+    // - Any ostreamable type can be logged with the stream insertion operator
+    // - Any ostreamable type can be logged with the function call operator
+    inline auto log(log_type _type) noexcept { return detail::log_impl(std::move(_type)); }
+
+    inline void log(log_type _type, std::string_view _message) noexcept { log(std::move(_type)) << std::move(_message); }
+
+    inline void set_log_stream(std::ostream& _stream) { detail::g_logStream.store(std::ref(_stream), std::memory_order_release); }
 
     inline auto info = log(log_type::INFO);
     inline auto warn = log(log_type::WARN);
