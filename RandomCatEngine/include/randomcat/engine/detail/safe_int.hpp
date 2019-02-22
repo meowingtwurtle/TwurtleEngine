48262027084d23c3492c2e956b064724be6394d4
Jason Cobb <jason.e.cobb@gmail.com>
2019-02-22 17:44:28 -0500

    Rework texture_array

--- RandomCatEngine/include/randomcat/engine/detail/safe_int.hpp
+++ RandomCatEngine/include/randomcat/engine/detail/safe_int.hpp
@@ -2,14 +2,27 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <ostream>
 
 namespace randomcat::engine::util_detail {
-    template<typename Tag>
+    template<typename Value, typename Tag>
     struct safe_integer {
-        std::int32_t value;
+        Value value;
 
-        bool operator==(safe_integer const& _other) const noexcept { return value == _other.value; }
+        template<typename = decltype(std::declval<Value const&>() == std::declval<Value const&>())>
+        bool operator==(safe_integer const& _other) const noexcept {
+            return value == _other.value;
+        }
 
-        bool operator!=(safe_integer const& _other) const noexcept { return !(*this == _other); }
+        template<typename = decltype(std::declval<Value const&>() != std::declval<Value const&>())>
+        bool operator!=(safe_integer const& _other) const noexcept {
+            return value != _other.value;
+        }
+
+        template<typename = decltype(std::declval<std::ostream&>() << std::declval<Value const&>())>
+        friend std::ostream& operator<<(std::ostream& _out, safe_integer const& _val) noexcept {
+            _out << _val.value;
+            return _out;
+        }
     };
 }    // namespace randomcat::engine::util_detail
