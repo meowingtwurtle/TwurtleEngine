5bdc0f9f5e8a5b018894564ceaaf08f3c29b8e32
Jason Cobb <jason.e.cobb@gmail.com>
2019-03-28 17:28:25 -0400

    Templatize render objects based on vertex type Special support is provided for default_vertex

--- RandomCatEngine/include/randomcat/engine/graphics/object.hpp
+++ RandomCatEngine/include/randomcat/engine/graphics/object.hpp
@@ -133,47 +133,168 @@ namespace randomcat::engine::graphics {
     using location_quad = render_object_detail::basic_quad<default_vertex::location>;
     using texture_quad = render_object_detail::basic_quad<default_vertex::texture>;
 
-    class render_object_triangle {
+    template<typename>
+    class render_object_triangle;
+
+    namespace render_object_detail {
+        template<typename Vertex>
+        class render_object_triangle_base {
+        public:
+            using vertex = Vertex;
+
+            render_object_triangle_base(vertex _first, vertex _second, vertex _third) noexcept
+            : m_vertices{std::move(_first), std::move(_second), std::move(_third)} {}
+
+            auto const& vertices() const noexcept { return m_vertices; }
+
+            RC_SUB_PARTS(vertices);
+
+            template<typename NewVertex, typename F>
+            auto use_vertex(F&& _f) const noexcept {
+                return render_object_triangle<NewVertex>(std::forward<F>(_f)(m_vertices[0]),
+                                                         std::forward<F>(_f)(m_vertices[1]),
+                                                         std::forward<F>(_f)(m_vertices[2]));
+            }
+
+        protected:
+            using container = std::array<vertex, 3>;
+            container m_vertices;
+        };
+    }    // namespace render_object_detail
+
+    template<typename Vertex = default_vertex>
+    class render_object_triangle : public render_object_detail::render_object_triangle_base<Vertex> {
+        using render_object_detail::render_object_triangle_base<Vertex>::render_object_triangle_base;
+    };
+
+    template<>
+    class render_object_triangle<> : public render_object_detail::render_object_triangle_base<default_vertex> {
     public:
         using vertex = default_vertex;
 
+        using render_object_detail::render_object_triangle_base<vertex>::render_object_triangle_base;
+
         render_object_triangle(location_triangle _locations, texture_triangle _texture) noexcept
-        : m_vertices{vertex{_locations[0], _texture[0]}, vertex{_locations[1], _texture[1]}, vertex{_locations[2], _texture[2]}} {
+        : render_object_detail::render_object_triangle_base<default_vertex>{vertex{_locations[0], _texture[0]},
+                                                                            vertex{_locations[1], _texture[1]},
+                                                                            vertex{_locations[2], _texture[2]}} {
             set_normal(impl_call, -glm::normalize(glm::cross(_locations[1].value - _locations[0].value, _locations[2].value - _locations[1].value)));
         }
-
-        auto const& vertices() const noexcept { return m_vertices; }
 
         void set_normal(impl_call_only, glm::vec3 _normal) noexcept {
             m_normal = _normal;
             std::for_each(begin(m_vertices), end(m_vertices), [&](auto& v) { v.normal = _normal; });
         }
 
-        RC_SUB_PARTS(vertices);
-
     private:
-        std::array<vertex, 3> m_vertices;
         glm::vec3 m_normal;
     };
 
-    class render_object_rectangle {
+    template<typename>
+    class render_object_rectangle;
+
+    namespace render_object_detail {
+        template<typename Vertex>
+        class render_object_rectangle_base {
+        public:
+            using vertex = Vertex;
+            using triangle = render_object_triangle<vertex>;
+
+            render_object_rectangle_base(vertex _first, vertex _second, vertex _third, vertex _fourth) noexcept
+            : m_triangles{{{_first, _second, _third}, {_first, _third, _fourth}}} {}
+
+            auto const& triangles() const noexcept { return m_triangles; }
+
+            RC_SUB_PARTS(triangles);
+
+            template<typename NewVertex, typename F>
+            auto use_vertex(F&& _f) const noexcept {
+                return render_object_rectangle<NewVertex>(impl_call,
+                                                          m_triangles[0].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                          m_triangles[1].template use_vertex<NewVertex>(std::forward<F>(_f)));
+            }
+
+            render_object_rectangle_base(impl_call_only, triangle _first, triangle _second) noexcept
+            : m_triangles{std::move(_first), std::move(_second)} {}
+
+        protected:
+            std::array<triangle, 2> m_triangles;
+        };
+    }    // namespace render_object_detail
+
+    template<typename Vertex = default_vertex>
+    class render_object_rectangle : public render_object_detail::render_object_rectangle_base<Vertex> {
     public:
-        render_object_rectangle(location_quad _locations, texture_quad _texture) noexcept
-        : m_triangles{render_object_triangle{location_triangle{_locations[0], _locations[1], _locations[2]},
-                                             texture_triangle{_texture[0], _texture[1], _texture[2]}},
-                      render_object_triangle{location_triangle{_locations[0], _locations[2], _locations[3]},
-                                             texture_triangle{_texture[0], _texture[2], _texture[3]}}} {}
+        using vertex = Vertex;
 
-        auto const& triangles() const noexcept { return m_triangles; }
-
-        RC_SUB_PARTS(triangles);
-
-    private:
-        std::array<render_object_triangle, 2> m_triangles;
+        using render_object_detail::render_object_rectangle_base<vertex>::render_object_rectangle_base;
     };
 
-    class render_object_rect_prism {
+    template<>
+    class render_object_rectangle<> : public render_object_detail::render_object_rectangle_base<default_vertex> {
     public:
+        using vertex = default_vertex;
+
+        using render_object_detail::render_object_rectangle_base<vertex>::render_object_rectangle_base;
+
+        render_object_rectangle(location_quad _locations, texture_quad _texture) noexcept
+        : render_object_detail::render_object_rectangle_base<
+            vertex>{impl_call,
+                    render_object_triangle<vertex>{location_triangle{_locations[0], _locations[1], _locations[2]},
+                                                   texture_triangle{_texture[0], _texture[1], _texture[2]}},
+                    render_object_triangle<vertex>{location_triangle{_locations[0], _locations[2], _locations[3]},
+                                                   texture_triangle{_texture[0], _texture[2], _texture[3]}}} {}
+    };
+
+    template<typename>
+    class render_object_rect_prism;
+
+    namespace render_object_detail {
+        template<typename Vertex>
+        class render_object_rect_prism_base {
+        public:
+            using vertex = Vertex;
+            using container = std::array<render_object_rectangle<vertex>, 6>;
+
+            auto const& sides() const noexcept { return m_rectangles; }
+            auto center() const noexcept { return m_center; }
+
+            RC_SUB_PARTS(sides);
+
+            template<typename NewVertex, typename F>
+            auto use_vertex(F&& _f) const noexcept {
+                return render_object_rect_prism<NewVertex>(impl_call,
+                                                           std::array<render_object_rectangle<NewVertex>,
+                                                                      6>{m_rectangles[0].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                                         m_rectangles[1].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                                         m_rectangles[2].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                                         m_rectangles[3].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                                         m_rectangles[4].template use_vertex<NewVertex>(std::forward<F>(_f)),
+                                                                         m_rectangles[5].template use_vertex<NewVertex>(std::forward<F>(_f))},
+                                                           m_center);
+            }
+
+            render_object_rect_prism_base(impl_call_only, container _container, glm::vec3 _center)
+            : m_rectangles(std::move(_container)), m_center(std::move(_center)) {}
+
+        protected:
+            container m_rectangles;
+            glm::vec3 m_center;
+        };
+    }    // namespace render_object_detail
+
+    template<typename Vertex = default_vertex>
+    class render_object_rect_prism : public render_object_detail::render_object_rect_prism_base<Vertex> {
+    public:
+        using vertex = Vertex;
+        using render_object_detail::render_object_rect_prism_base<vertex>::render_object_rect_prism_base;
+    };
+
+    template<>
+    class render_object_rect_prism<> : public render_object_detail::render_object_rect_prism_base<default_vertex> {
+    public:
+        using vertex = default_vertex;
+
         explicit render_object_rect_prism(glm::vec3 _center, glm::vec3 _sides, texture_array_index _texture) noexcept
         : render_object_rect_prism(_center, _sides, _texture, _texture, _texture, _texture, _texture, _texture) {}
 
@@ -185,22 +306,21 @@ namespace randomcat::engine::graphics {
                                           texture_array_index _texLY,
                                           texture_array_index _texHZ,
                                           texture_array_index _texLZ) noexcept
-        : m_rectangles(gen_triangles(_center, _sides, _texHX, _texLX, _texHY, _texLY, _texHZ, _texLZ)), m_center(_center) {}
-
-        auto const& sides() const noexcept { return m_rectangles; }
-        auto center() const noexcept { return m_center; }
-
-        RC_SUB_PARTS(sides);
+        : render_object_detail::render_object_rect_prism_base<default_vertex>(impl_call,
+                                                                              gen_triangles(_center, _sides, _texHX, _texLX, _texHY, _texLY, _texHZ, _texLZ),
+                                                                              _center) {}
 
     private:
-        std::array<render_object_rectangle, 6> gen_triangles(glm::vec3 _center,
-                                                             glm::vec3 _sides,
-                                                             texture_array_index _texHX,
-                                                             texture_array_index _texLX,
-                                                             texture_array_index _texHY,
-                                                             texture_array_index _texLY,
-                                                             texture_array_index _texHZ,
-                                                             texture_array_index _texLZ) const noexcept {
+        using rectangle = render_object_rectangle<vertex>;
+
+        std::array<rectangle, 6> gen_triangles(glm::vec3 _center,
+                                               glm::vec3 _sides,
+                                               texture_array_index _texHX,
+                                               texture_array_index _texLX,
+                                               texture_array_index _texHY,
+                                               texture_array_index _texLY,
+                                               texture_array_index _texHZ,
+                                               texture_array_index _texLZ) const noexcept {
             auto vecHX = glm::vec3{_sides.x / 2, 0, 0};
             auto vecHY = glm::vec3{0, _sides.y / 2, 0};
             auto vecHZ = glm::vec3{0, 0, _sides.z / 2};
@@ -232,24 +352,32 @@ namespace randomcat::engine::graphics {
                 return texture_quad{{{0, 0}, layer}, {{1, 0}, layer}, {{1, 1}, layer}, {{0, 1}, layer}};
             };
 
-            auto rectHZ = render_object_rectangle{location(pointLXHYHZ, pointHXHYHZ, pointHXLYHZ, pointLXLYHZ), texture(_texHZ)};
-            auto rectLZ = render_object_rectangle{location(pointHXHYLZ, pointLXHYLZ, pointLXLYLZ, pointHXLYLZ), texture(_texLZ)};
+            auto rectHZ = rectangle{location(pointLXHYHZ, pointHXHYHZ, pointHXLYHZ, pointLXLYHZ), texture(_texHZ)};
+            auto rectLZ = rectangle{location(pointHXHYLZ, pointLXHYLZ, pointLXLYLZ, pointHXLYLZ), texture(_texLZ)};
 
-            auto rectHX = render_object_rectangle{location(pointHXHYHZ, pointHXHYLZ, pointHXLYLZ, pointHXLYHZ), texture(_texHX)};
-            auto rectLX = render_object_rectangle{location(pointLXHYLZ, pointLXHYHZ, pointLXLYHZ, pointLXLYLZ), texture(_texLX)};
+            auto rectHX = rectangle{location(pointHXHYHZ, pointHXHYLZ, pointHXLYLZ, pointHXLYHZ), texture(_texHX)};
+            auto rectLX = rectangle{location(pointLXHYLZ, pointLXHYHZ, pointLXLYHZ, pointLXLYLZ), texture(_texLX)};
 
-            auto rectHY = render_object_rectangle{location(pointHXHYHZ, pointLXHYHZ, pointLXHYLZ, pointHXHYLZ), texture(_texHY)};
-            auto rectLY = render_object_rectangle{location(pointHXLYLZ, pointLXLYLZ, pointLXLYHZ, pointHXLYHZ), texture(_texLY)};
+            auto rectHY = rectangle{location(pointHXHYHZ, pointLXHYHZ, pointLXHYLZ, pointHXHYLZ), texture(_texHY)};
+            auto rectLY = rectangle{location(pointHXLYLZ, pointLXLYLZ, pointLXLYHZ, pointHXLYHZ), texture(_texLY)};
 
             return {rectHZ, rectLZ, rectHY, rectLY, rectLX, rectHX};
         }
-
-        std::array<render_object_rectangle, 6> m_rectangles;
-        glm::vec3 m_center;
     };
 
-    class render_object_cube : public render_object_rect_prism {
+    template<typename Vertex = default_vertex>
+    class render_object_cube : public render_object_rect_prism<Vertex> {
     public:
+        using vertex = Vertex;
+
+        render_object_cube() = delete;
+    };
+
+    template<>
+    class render_object_cube<> : public render_object_rect_prism<default_vertex> {
+    public:
+        using vertex = default_vertex;
+
         explicit render_object_cube(glm::vec3 _center, GLfloat _side, texture_array_index _texture) noexcept
         : render_object_cube(std::move(_center), std::move(_side), _texture, _texture, _texture, _texture, _texture, _texture) {}
 
@@ -271,33 +399,79 @@ namespace randomcat::engine::graphics {
                                    std::move(_texLZ)) {}
     };
 
-    class render_object_regular_polygon {
+    template<typename>
+    class render_object_regular_polygon;
+
+    namespace render_object_detail {
+        template<typename Vertex>
+        class render_object_regular_polygon_base {
+        public:
+            using vertex = Vertex;
+            using triangle = render_object_triangle<vertex>;
+            using container = std::vector<triangle>;
+
+            auto const& triangles() const noexcept { return m_triangles; }
+            auto side_count() const noexcept { return m_sideCount; }
+
+            RC_SUB_PARTS(triangles);
+
+            render_object_regular_polygon_base(impl_call_only, container _triangles, std::int32_t _sideCount) noexcept
+            : m_triangles(std::move(_triangles)), m_sideCount(std::move(_sideCount)) {}
+
+            template<typename NewVertex, typename F>
+            auto use_vertex(F&& _f) const noexcept {
+                std::vector<render_object_triangle<NewVertex>> newTriangles;
+                newTriangles.reserve(side_count());
+
+                std::transform(begin(m_triangles), end(m_triangles), std::back_inserter(newTriangles), [&](auto const& tri) {
+                    return tri.template use_vertex<NewVertex>(std::forward<F>(_f));
+                });
+
+                return render_object_regular_polygon<NewVertex>(impl_call, newTriangles, m_sideCount);
+            }
+
+        protected:
+            container m_triangles;
+            std::int32_t m_sideCount;
+        };
+    }    // namespace render_object_detail
+
+    template<typename Vertex = default_vertex>
+    class render_object_regular_polygon : public render_object_detail::render_object_regular_polygon_base<Vertex> {
     public:
-        render_object_regular_polygon(int sides, glm::vec3 center, float radius, texture_array_index _texture)
-        : m_sideCount(std::move(sides)) {
-            for (int i = 0; i < sides; ++i) {
-                auto theta0 = units::degrees(360.0f * i / sides);
-                auto theta1 = units::degrees(360.0f * (i + 1) / sides);
-                auto point0 = center + radius * glm::vec3(cos(theta0), sin(theta0), 0);
-                auto point1 = center + radius * glm::vec3(cos(theta1), sin(theta1), 0);
+        using vertex = Vertex;
+        using render_object_detail::render_object_regular_polygon_base<vertex>::render_object_regular_polygon_base;
+    };
+
+    template<>
+    class render_object_regular_polygon<> : public render_object_detail::render_object_regular_polygon_base<default_vertex> {
+    public:
+        using vertex = default_vertex;
+        using render_object_detail::render_object_regular_polygon_base<vertex>::render_object_regular_polygon_base;
+
+        render_object_regular_polygon(int _sides, glm::vec3 _center, float _radius, texture_array_index _texture)
+        : render_object_detail::render_object_regular_polygon_base<default_vertex>(impl_call, gen_triangles(_sides, _center, _radius, _texture), _sides) {}
+
+    private:
+        static std::vector<render_object_triangle<vertex>> gen_triangles(std::int32_t _sides, glm::vec3 _center, float _radius, texture_array_index _texture) noexcept {
+            std::vector<render_object_triangle<vertex>> triangles;
+
+            for (int i = 0; i < _sides; ++i) {
+                auto theta0 = units::degrees(360.0f * i / _sides);
+                auto theta1 = units::degrees(360.0f * (i + 1) / _sides);
+                auto point0 = _center + _radius * glm::vec3(cos(theta0), sin(theta0), 0);
+                auto point1 = _center + _radius * glm::vec3(cos(theta1), sin(theta1), 0);
 
                 auto texturePoint = [&](auto const& theta) {
                     return default_vertex::texture{{(cos(theta) + 1.f) / 2.f, (1 - sin(theta)) / 2.f}, _texture};
                 };
 
-                m_triangles.push_back(render_object_triangle(location_triangle{{center}, {point0}, {point1}},
-                                                             texture_triangle{{{0.5, 0.5}, _texture}, texturePoint(theta0), texturePoint(theta1)}));
+                triangles.push_back(render_object_triangle<vertex>(location_triangle{{_center}, {point0}, {point1}},
+                                                                   texture_triangle{{{0.5, 0.5}, _texture}, texturePoint(theta0), texturePoint(theta1)}));
             }
+
+            return triangles;
         }
-
-        auto const& triangles() const noexcept { return m_triangles; }
-        auto side_count() const noexcept { return m_sideCount; }
-
-        RC_SUB_PARTS(triangles);
-
-    private:
-        std::vector<render_object_triangle> m_triangles;
-        int m_sideCount;
     };
 
 #undef RC_SUB_PARTS
